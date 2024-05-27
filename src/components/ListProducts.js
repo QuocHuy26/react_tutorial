b629e7c5054146a41add1ed7898f64a550609b72
quochuy262 <do.huy2607@gmail.com>
2024-05-28 00:27:14 +0700

    use json-server, get data using api

--- src/components/ListProducts.js
+++ src/components/ListProducts.js
@@ -1,23 +1,24 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
+import axios from 'axios';
 
 const ListProducts = () => {
-    const list = [
-        {
-            name: 'Bánh mỳ',
-            quantity: '10',
-            unit_price: '10000',
-        },
-        {
-            name: 'Sữa',
-            quantity: '13',
-            unit_price: '6000',
-        },
-        {
-            name: 'Nước ngọt',
-            quantity: '20',
-            unit_price: '16000',
-        },
-    ];
+    // const list = [
+    //     {
+    //         name: 'Bánh mỳ',
+    //         quantity: '10',
+    //         unit_price: '10000',
+    //     },
+    //     {
+    //         name: 'Sữa',
+    //         quantity: '13',
+    //         unit_price: '6000',
+    //     },
+    //     {
+    //         name: 'Nước ngọt',
+    //         quantity: '20',
+    //         unit_price: '16000',
+    //     },
+    // ];
 
     const [listProduct, setListProduct] = useState();
     const [id, setId] = useState('0');
@@ -28,6 +29,7 @@ const ListProducts = () => {
     const [showUpdate, setShowUpdate] = useState(false);
     const [showDetail, setShowDetail] = useState(false);
     const [showAdd, setShowAdd] = useState(false);
+    const [tableFlag, setTableFlag] = useState(false);
 
     const handleShowDetail = (product) =>{
         setProductDetail(product);
@@ -40,7 +42,7 @@ const ListProducts = () => {
     }
 
     const handleShowUpdate = (product) =>{
-        setId(product.key);
+        setId(product.id);
         setNewName(product.name);
         setNewQuantity(product.quantity);
         setNewPrice(product.unit_price);
@@ -50,22 +52,39 @@ const ListProducts = () => {
         setProductDetail();
         setShowUpdate(false);
     }
-    const handleUpdate = (id) => {
-        const newList = [...listProduct];
-        const targetItem = newList.find(item => item.key === id);
-        targetItem.name = newName;
-        targetItem.quantity = newQuantity;
-        targetItem.unit_price = newPrice;
-        setListProduct([...newList]);
-        setShowUpdate(false);
+    const handleUpdate = async (id) => {
+        // const newList = [...listProduct];
+        // const targetItem = newList.find(item => item.key === id);
+        // targetItem.name = newName;
+        // targetItem.quantity = newQuantity;
+        // targetItem.unit_price = newPrice;
+        // setListProduct([...newList]);
+        const data = {
+            name: newName,
+            quantity: newQuantity,
+            unit_price: newPrice,
+        };
+        try {
+            await axios.put(`http://localhost:3001/products/${id}`, data);
+            setShowUpdate(false);
+            setTableFlag((tableFlag) => !tableFlag);
+        } catch (error) {
+            console.error('Error updating data: ', error);
+        }
     }
 
-    const handleDelete = (key) => {
-        const newList = listProduct.filter(item => item.key !== key);
-        while (key <= newList.length) {
-            newList[key-1].key = key ++;
-        };
-        setListProduct([...newList]);
+    const handleDelete = async (id) => {
+        // const newList = listProduct.filter(item => item.key !== key);
+        // while (key <= newList.length) {
+        //     newList[key-1].key = key ++;
+        // };
+        // setListProduct([...newList]);
+        try {
+            await axios.delete(`http://localhost:3001/products/${id}`);
+            setTableFlag((tableFlag) => !tableFlag);
+        } catch (error) {
+            console.error('Error deleting data: ', error);
+        }
     }
 
     const handleShowAdd = () => {
@@ -74,25 +93,80 @@ const ListProducts = () => {
     const handleCancelShowAdd = () => {
         setShowAdd(false);
     }
-    const handleAdd = () => {
+    const handleAdd = async () => {
         const newProduct = {
             key: listProduct.length + 1,
             name: newName,
             quantity: newQuantity,
             unit_price: newPrice,
         }
-        const newList = [...listProduct, newProduct];
-        setListProduct([...newList]);
+        // const newList = [...listProduct, newProduct];
+        // setListProduct([...newList]);
+        try {
+            await axios.post(`http://localhost:3001/products`, newProduct);
+            setTableFlag((tableFlag) => !tableFlag);
+        } catch (error) {
+            console.error('Error adding data: ', error);
+        }
         setShowAdd(false);
     }
 
     useEffect(() => {
-        let key = 1;
-        list.forEach((item) => {
-            item.key = key ++;
-        });
-        setListProduct([...list]);
-    }, []);
+        // let key = 1;
+        // list.forEach((item) => {
+        //     item.key = key ++;
+        // });
+        // setListProduct([...list]);
+        axios.get('http://localhost:3001/products')
+            .then(response => {
+                let key = 1;
+                const list = response.data.map((item) => {
+                    item.key = key ++;
+                    return item;
+                });
+                console.log(list);
+                setListProduct([...list]);
+            })
+    }, [tableFlag]);
+
+    const Product = React.memo (({item})=>{
+        console.log('rendering: ', item.name);
+        return (
+            <>
+                <p style={{width:'100%', margin: '3px 0', borderTop: '1px solid #666',
+                                        display: 'flex', alignItems:'center',}}
+                >
+                    <span style={{width: '100px'}}>{item.key}</span>
+                    <span style={{width: '200px'}}>{item.name}</span>
+                    <span style={{width: '150px'}}>{item.quantity}</span>
+                    <span style={{width: '150px'}}>{item.unit_price}</span>
+                    <span
+                        style={{width: '80px', color: '#0f5', cursor:'pointer',}}
+                        onClick = {()=>handleShowDetail(item)}
+                    >
+                        Xem
+                    </span>
+                    <span
+                        style={{width: '80px', color: 'blue', cursor:'pointer',}}
+                        onClick = {()=>handleShowUpdate(item)}
+                    >
+                        Sửa
+                    </span>
+                    <span
+                        style={{width: '80px', color: 'red', cursor:'pointer',}}
+                        onClick = {()=>handleDelete(item.id)}
+                    >
+                        Xóa
+                    </span>
+                </p>
+            </>
+        )
+    }, 
+    (prevProp, nextProp) =>{
+        return (prevProp.item.name === nextProp.item.name &&
+                prevProp.item.quantity === nextProp.item.quantity &&
+                prevProp.item.unit_price === nextProp.item.unit_price);
+    });
     return(
         <div>
             <button
@@ -118,7 +192,7 @@ const ListProducts = () => {
                 {listProduct? (listProduct.length? (listProduct.map((item) =>{
                     return (
                         <>
-                            <p style={{width:'100%', margin: '3px 0', borderTop: '1px solid #666',
+                            {/* <p style={{width:'100%', margin: '3px 0', borderTop: '1px solid #666',
                                         display: 'flex', alignItems:'center',}}
                             >
                                 <span style={{width: '100px'}}>{item.key}</span>
@@ -139,11 +213,12 @@ const ListProducts = () => {
                                 </span>
                                 <span
                                     style={{width: '80px', color: 'red', cursor:'pointer',}}
-                                    onClick = {()=>handleDelete(item.key)}
+                                    onClick = {()=>handleDelete(item.id)}
                                 >
                                     Xóa
                                 </span>
-                            </p>
+                            </p> */}
+                            <Product key={item.id} item={item}/>
                             {showAdd && (
                                 <div style={{width:'100%', height:'100%', position: 'absolute', left: '0', top: '0',backgroundColor:'gray',}}>
                                     <div style={{width:'50%', padding: '5px',position: 'absolute', left: '50%', top: '-50%',
