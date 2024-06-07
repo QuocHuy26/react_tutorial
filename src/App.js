9098e080e0009d76bc84ed3bb2b75d026976aeae
quochuy262 <do.huy2607@gmail.com>
2024-06-08 00:04:54 +0700

    protect private route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import AppRoutes from './AppRoutes';
+import AppRoutes from './routes/AppRoutes';
 
 function App() {
   return (
---
+++ src/routes/PrivateRoute.js
@@ -0,0 +1,25 @@
+import React, {useEffect} from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate} from 'react-router-dom'
+
+const PrivateRoute = (props) => {
+    const user = useSelector(state => state.user);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (user.token === '') {
+            navigate('/');
+        }
+    }, [user.token, navigate]);
+    
+    if (user.token === '') {
+        return null;
+    }
+
+    return (
+        <>
+            {props.children}
+        </>
+)
+}
+export default PrivateRoute;
