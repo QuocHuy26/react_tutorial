c7bb713ec4e3b677220d0add3a12333cabb3af6e
quochuy262 <do.huy2607@gmail.com>
2024-06-26 20:19:51 +0700

    create rootReducer

--- src/store/reduxPersist.js
+++ src/store/reduxPersist.js
@@ -2,14 +2,15 @@ import { configureStore } from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
-import userReducer from './reducers/user';
+import rootReducer from './reducers';
 
 const persistConfig = {
   key: 'root',
   storage,
+  whitelist: ['user'],  //chỉ lưu trữ state của userReducer
 };
 
-const persistedReducer = persistReducer(persistConfig, userReducer);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({reducer: persistedReducer});
 export const persistor = persistStore(store);
